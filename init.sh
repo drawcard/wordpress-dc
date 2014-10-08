5fd3f3c7dd0e08aa777bb18983b9625d58c59379
Chris Drabsch <cd@drawcard.com.au>
2014-10-08 17:37:39 +1030

    Updated init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,9 @@ git checkout -b 'dev'
 echo "Adding submodules..."
 git submodule add https://github.com/BrunoDeBarros/git-deploy-php deploy/git-deploy-php
 git submodule add https://github.com/jplew/SyncDB deploy/SyncDB
-git submodule add https://github.com/roots/roots content/themes/build
+
+#Tracking branch 7.0.0 - update for each new roots release
+git submodule add -b 7.0.0 https://github.com/roots/roots-sass content/themes/build 
 
 echo "Pull from repo and fetch latest submodules..."
 git pull origin master && git submodule update --init --recursive
