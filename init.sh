7f6ba16068c897f8881129ff296909bafd10d198
Chris Drabsch <cd@drawcard.com.au>
2014-10-23 15:56:53 +1030

    Updated init.sh to remove old branch reference, now that Roots v7.0.1 is available on master

--- init.sh
+++ init.sh
@@ -6,8 +6,7 @@ echo "Adding submodules..."
 git submodule add https://github.com/BrunoDeBarros/git-deploy-php deploy/git-deploy-php
 git submodule add https://github.com/jplew/SyncDB deploy/SyncDB
 
-#Tracking branch 7.0.0 - update for each new roots release
-git submodule add -b 7.0.0 https://github.com/roots/roots-sass content/themes/build 
+git submodule add https://github.com/roots/roots-sass content/themes/build 
 
 echo "Pull from repo and fetch latest submodules..."
 git pull origin master && git submodule update --init --recursive
